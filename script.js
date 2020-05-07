fd12327ba748946605ab8f502f395b31d3ab95ef
Bálint <aprobalint98@gmail.com>
2020-05-07 20:17:59 +0200

    coin hátterének, kommentek hozzáfűzése, pacman mozgása, apróbb módosítások

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let blockSize = 30;
-let gameArea;
-let score = 0;
-let lives = 3;
-let myMusic;
-let mySound;
-let field = [
+const blockSize = 30;           //blokkok méretei
+let gameArea;                   //játéktér
+let score = 0;                  //pontszám
+let lives = 3;                  //életek
+let myMusic;                    //háttérzene
+let mySound;                    //életvesztés hangeffektje
+const field = [                 //pálya tömb
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,2,2,2,2,2,2,2,2,2,2,2,2,0,0,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,0,0,2,0,0,0,0,0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2,0],
     [0,2,0,0,0,0,2,0,0,0,0,0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2,0],
     [0,2,0,0,0,0,2,0,0,0,0,0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2,0],
     [0,2,0,0,0,0,2,0,0,0,0,0,2,0,0,2,0,0,0,0,0,2,0,0,0,0,2,0],
@@ -18,9 +19,9 @@ let field = [
     [3,3,3,3,3,0,2,0,0,0,0,0,3,0,0,3,0,0,0,0,0,2,0,3,3,3,3,3],
     [3,3,3,3,3,0,2,0,0,3,3,3,3,3,3,3,3,3,3,0,0,2,0,3,3,3,3,3],
     [3,3,3,3,3,0,2,0,0,3,0,0,0,3,3,0,0,0,3,0,0,2,0,3,3,3,3,3],
-    [3,3,3,3,3,0,2,0,0,3,0,3,3,3,3,3,3,0,3,0,0,2,0,3,3,3,3,3],
-    [3,3,3,3,3,0,2,3,3,3,0,3,3,3,3,3,3,0,3,3,3,2,0,3,3,3,3,3],
-    [3,3,3,3,3,0,2,0,0,3,0,3,3,4,3,3,3,0,3,0,0,2,0,3,3,3,3,3],
+    [0,0,0,0,0,0,2,0,0,3,0,3,3,3,3,3,3,0,3,0,0,2,0,0,0,0,0,0],
+    [3,3,3,3,3,3,2,3,3,3,0,3,3,3,3,3,3,0,3,3,3,2,3,3,3,3,3,3],
+    [0,0,0,0,0,0,2,0,0,3,0,3,3,4,3,3,3,0,3,0,0,2,0,0,0,0,0,0],
     [3,3,3,3,3,0,2,0,0,3,0,0,0,0,0,0,0,0,3,0,0,2,0,3,3,3,3,3],
     [3,3,3,3,3,0,2,0,0,3,3,3,3,3,3,3,3,3,3,0,0,2,0,3,3,3,3,3],
     [3,3,3,3,3,0,2,0,0,3,0,0,0,0,0,0,0,0,3,0,0,2,0,3,3,3,3,3],
@@ -37,14 +38,19 @@ let field = [
     [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ]
-let pacman = {
+const mapSize = {               //pálya mérete
+    x:27,
+    y:29
+}
+let pacman = {                  //pacman kiinduló pontja
     x:14,
     y:23
 }
-let ghost = {
+let ghost = {                   //szellem kiinduló pontja
     x:13,
     y:15
 }
+//pálya tömb bejárása, blokkok hozzáadása, osztály vagy id hozzáadása a blokkokhoz, majd az egész hozzáadása ajátéktérhez
 function drawGameArea(){
    for(let x = 0; x < field.length; x++){
        for (let y = 0;y < field[x].length; y++){
@@ -68,9 +74,22 @@ function drawGameArea(){
        }
    }
 }
-
-function movePacmanToOtherPosition(e) {
-    let key = e.key;
+let key = 'd';          //key alapértelemezett beállítása jobbra/ d billentyűre
+//irány bekérése
+function setPacmanDirection(e) {
+    key = e.key;
+}
+//pacman mozgatása
+function movePacmanToOtherPosition() {
+    //ha a pacman bal oldalt kimenne a pályáról, akkor jobbról jöjjön be (valamiért nem működik, nem tudtam rájönni miért nem)
+    if(pacman.x === 0){
+        pacman.x = mapSize.x;
+        animatePac();
+    }
+    //ha a pacman jobb oldalt kimenne a pályáról, akkor balról jöjjön be
+    if(pacman.x === mapSize.x){
+        pacman.x = 0;
+    }
     if (key === 'w') {
             if (field[pacman.y - 1][pacman.x]) {
                 pacman.y = pacman.y - 1;
@@ -96,7 +115,7 @@ function movePacmanToOtherPosition(e) {
         }
     }
 }
-
+//szellem mozgatása - kicsit bután, random számot kap és a szám alapján elindul a hozzárendelt irányba
 function movingGhost(){
     if(lives > 0){
         let ghostDirection = Math.floor(Math.random() * 4 + 1);
@@ -112,29 +131,25 @@ function movingGhost(){
         animateGhost();
     }
 }
+//szellem animálása
 function animateGhost() {
     $('#ghost').animate({
         top: ghost.y * blockSize,
         left: ghost.x * blockSize
     }, 0);
 }
-function distance(pacman, ghost){
-    dx = pacman.x - ghost.x;
-    dy = pacman.y - ghost.y;
-    return Math.sqrt(dx*dx + dy*dy);
-}
-
+//pacman animálása
 function animatePac(){
     $('#pacman').animate({
         top: pacman.y * blockSize,
         left: pacman.x * blockSize
     }, 1, function () {
-        gameArea.find('.coin').each(function () {
+        gameArea.find('.coin').each(function () {                           //ha coint talál, akkor "felveszi" és kap 10 pontot
             if ($(this).css('top') === $('#pacman').css('top')
                 && $(this).css('left') === $('#pacman').css('left')) {
                 $(this).removeClass('coin');
                 score += 10;
-                if(score === 2450){
+                if(score === 2450){     //ha a pontszám eléri a maxot, akkor a játékos nyert és hozzáadódik a ranglistához
                     YouWonThisGame();
                     var person = prompt("Adja meg a nevét:", "Anonym szeretnék maradni!");
                     localStorage.setItem(person, Number(score));
@@ -143,6 +158,7 @@ function animatePac(){
                 $('#score').text(score);
             }
         });
+        //ha szellemet talál a pacman, akkor életet veszít, ha minden életetét elveszítette akkor a játék véget ér és hozzáadódik a ranglistához
         gameArea.find('#ghost').each(function () {
             if($(this).css('top') === $('#pacman').css('top')
                 && $(this).css('left') === $('#pacman').css('left')) {
@@ -169,6 +185,7 @@ function animatePac(){
         });
     });
 }
+//hangeffekt definiálása
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -176,34 +193,54 @@ function sound(src) {
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function () {
         this.sound.play();
     }
-    this.stop = function(){
+    this.stop = function () {
         this.sound.pause();
     }
 }
+//háttérzene definiálása
+function backgroundMusic(src) {
+    this.backgroundMusic = document.createElement("audio");
+    this.backgroundMusic.loop = true;
+    this.backgroundMusic.src = src;
+    this.backgroundMusic.setAttribute("preload", "auto");
+    this.backgroundMusic.setAttribute("controls", "none");
+    this.backgroundMusic.style.display = "none";
+    document.body.appendChild(this.backgroundMusic);
+    this.play = function(){
+        this.backgroundMusic.play();
+    }
+    this.stop = function(){
+        this.backgroundMusic.pause();
+    }
+}
+//játék / új játék kezdése amennyiben a New Game gombra rákattintunk
 function startNewGame() {
-    $('#map').css("background-image", "none");
+    $('#map').css("background-image", "none");          //kezdő oldal beállításainak eltűntetése
     $('#map').css("margin-left", "500px");
     $('#main_score').css("display","none");
     $('#newGame').css("display","none");
     $('#list').css("display","none");
 
-    drawGameArea();
-    mySound = new sound("losing_lives_music.mp3");
-    myMusic = new sound("original_music.mp3");
-    myMusic.play();
-    setInterval(movingGhost, 100);
+    drawGameArea();                                     //játéktér kirajzolása
+    mySound = new sound("losing_lives_music.mp3");      //hangeffekt betöltése
+    myMusic = new backgroundMusic("original_music.mp3");    //háttérzene betöltése
+    myMusic.play();     //háttérzene elindítása
 
-    window.addEventListener('keydown', movePacmanToOtherPosition, false);
+    setInterval(movePacmanToOtherPosition, 100);  //pacman folytonos mozgatása a megfelelő irányba
+    setInterval(movingGhost, 100);                //szellem folytonos mozgatása
 
+    window.addEventListener('keydown', setPacmanDirection, false);
+
+    //játéktérhez hozzárendeljük a pontszám és élet mezőket, a hozzájuk tartozó id-kkal és css-el
     gameArea.append('<div id="score_tab">Score:<span id="score" style="padding-left: 10px"></span></div>')
     $('#score').text(score);
     gameArea.append('<div id="lives_tab">Lives:<span id="lives" style="padding-left: 10px"></span></div>')
     $('#lives').text(lives);
 }
-
+//függvény a játék elvesztéséhez
 function YouLostThisGame(){
     gameArea.empty();
     myMusic.stop();
@@ -211,12 +248,13 @@ function YouLostThisGame(){
     gameArea.append('<button id="newGame">Try Again</button>');
     $("#newGame").on('click', startNewGame);
 }
+//függvény a játék megnyeréséhez
 function YouWonThisGame(){
     gameArea.empty();
     myMusic.stop();
     gameArea.append('<div id="winner">WIN</div style>');
 }
-
+//toplista kirajzolása és feltöltése
 function FillToplist() {
     gameArea.empty();
     gameArea.append('<h2>Top 10</h2>');
@@ -242,7 +280,7 @@ function FillToplist() {
     }
 }
 
-
+//az oldal elindításakor betöltődő dolgok, játéktér hozzáadása, New Game és Scores gombok
 $(function () {
     gameArea = $('<div></div>');
     gameArea.appendTo('body');
